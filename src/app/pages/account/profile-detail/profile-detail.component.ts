29c9bdf8a6986562bf0c9860cd2f1b3a670ed36d
Priyanshu Chouhan <priyanschouhan123@gmail.com>
2023-12-06 20:51:04 +0530

    UI: Accounts page added

---
+++ src/app/pages/account/profile-detail/profile-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './profile-detail.component.html',
+  styleUrl: './profile-detail.component.css'
+})
+export class ProfileDetailComponent {
+
+}
