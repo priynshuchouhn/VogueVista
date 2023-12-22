2c3f52ba20886eaf1cc496a604c9280ea6b33afb
Priyanshu Chouhan <priyanschouhan123@gmail.com>
2023-12-22 11:30:42 +0530

    Search Toggle Created

--- src/app/shared/model/product.model.ts
+++ src/app/shared/model/product.model.ts
@@ -1,1 +1,17 @@
-export class Product {}
+export class Product {
+    productId: number;
+    name: string;
+    description: string;
+    price: number;
+    stockQuantity: number;
+    categoryId: number;
+
+    constructor(productId: number, name: string, description: string, price: number, stockQuantity: number, categoryId: number) {
+        this.productId = productId;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.stockQuantity = stockQuantity;
+        this.categoryId = categoryId;
+    }
+}
