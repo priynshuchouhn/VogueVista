a928573d7ac7af7fafe4462f20cf26b1bccd9927
Priyanshu Chouhan <priyanschouhan123@gmail.com>
2024-01-06 00:26:54 +0530

    App: Minor Fixes

--- src/app/pages/product/product-detail/product-detail.component.ts
+++ src/app/pages/product/product-detail/product-detail.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectorRef, Component, ViewChild } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { SlickCarouselComponent } from 'ngx-slick-carousel';
 import { Product } from 'src/app/shared/model/product.model';
@@ -11,10 +11,10 @@ import { Product } from 'src/app/shared/model/product.model';
 export class ProductDetailComponent {
   product!: Product
   similarProducts: Product[] = []
-  
 
 
-  constructor(private route: ActivatedRoute){
+
+  constructor(private route: ActivatedRoute) {
     this.route.data.subscribe(res => {
       this.product = res['productDetailData']['product'];
       this.similarProducts = res['productDetailData']['similarProducts']
@@ -24,27 +24,27 @@ export class ProductDetailComponent {
 
   trackByFn(index: number, item: any): any {
     return item; // Replace 'uniqueIdentifier' with an actual property of your data that provides a unique identifier.
- }
+  }
 
-  slideConfig = {"slidesToShow": 1, "slidesToScroll": 1, dots: true, prevArrow: false, nextArrow: false,draggable: true, asNavFor:'.thumbnail'}
-  slideVerticalConfig = {"slidesToShow": 3, "slidesToScroll": 1, infinite:true, prevArrow: false, nextArrow: false, vertical:true, asNavFor:'.main-slider', draggable: true,verticalSwiping:true, focusOnSelect: true}
- 
+  slideConfig = { "slidesToShow": 1, "slidesToScroll": 1, dots: true, prevArrow: false, nextArrow: false, draggable: true, asNavFor: '.thumbnail' }
+  slideVerticalConfig = { "slidesToShow": 3, "slidesToScroll": 1, infinite: true, prevArrow: false, nextArrow: false, vertical: true, asNavFor: '.main-slider', draggable: true, verticalSwiping: true, focusOnSelect: true }
+
   @ViewChild('slickMain') slickMain!: SlickCarouselComponent;
   @ViewChild('slickThumbnail') slickThumbnail!: SlickCarouselComponent;
 
-// When the slide data changes
-onDataChange() {
-  if (this.slickMain) {
-    this.slickMain.unslick();
-    setTimeout(()=>{
-      this.slickMain.initSlick();
-    },500)
+  // When the slide data changes
+  onDataChange() {
+    if (this.slickMain) {
+      this.slickMain.unslick();
+      setTimeout(() => {
+        this.slickMain.initSlick();
+      }, 500)
+    }
+    if (this.slickThumbnail) {
+      this.slickThumbnail.unslick();
+      setTimeout(() => {
+        this.slickThumbnail.initSlick();
+      }, 500)
+    }
   }
-  if (this.slickThumbnail) {
-    this.slickThumbnail.unslick();
-    setTimeout(()=>{
-      this.slickThumbnail.initSlick();
-    },500)
-  }
-}
 }
