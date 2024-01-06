29f6378803002f70ce29247ca3862a7fb50ed37b
Priyanshu Chouhan <priyanschouhan123@gmail.com>
2024-01-06 12:36:03 +0530

    App: Service worker added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,12 +23,12 @@ import { HttpClientModule, provideHttpClient, withFetch } from '@angular/common/
     NgbModule,
     HttpClientModule,
     ReactiveFormsModule,
-    // ServiceWorkerModule.register('ngsw-worker.js', {
-    //   enabled: !isDevMode(),
-    //   // Register the ServiceWorker as soon as the application is stable
-    //   // or after 30 seconds (whichever comes first).
-    //   registrationStrategy: 'registerWhenStable:30000'
-    // })
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [
     provideHttpClient(withFetch()),
