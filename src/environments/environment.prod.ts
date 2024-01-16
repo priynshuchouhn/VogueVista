49ca958410d2cdf2dc6925e16946fee4dc647d4d
Priyanshu Chouhan <priyanschouhan123@gmail.com>
2024-01-16 22:28:40 +0530

    App: API URL updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    API_URL: 'https://vogue-vista.onrender.com/api/',
+    API_URL: 'https://api.voguevista.live/api/',
     STRIPE_PUBLIC_KEY:'pk_test_51OXrXnSCO6d0uM2qbjAbtEdhjUuy8T9HaFwTL5upsD0r7P6KnbtnaZXSaTtiKYwiR8HyI7IDHijTbk9z4LllKlhB00m0PRTGWG'
   };
